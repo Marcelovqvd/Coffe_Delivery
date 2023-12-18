e5049a436ee5cb9ab13fc69ab391f02968343b0f
Marcelo <marcelovqvd@gmail.com>
2023-12-18 17:18:40 -0300

    router

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { DefaultLayout } from "./layouts/DefaultLayout.tsx";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
