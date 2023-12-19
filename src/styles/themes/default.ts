3dba1d22e4cd7f06700925f0a1478b22fccfb019
Marcelo <marcelovqvd@gmail.com>
2023-12-18 21:30:37 -0300

    theme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,46 @@
 export const defaultTheme = {
-  // base
-  white: "#FFFFFF",
-  "back-ground": "#FAFAFA",
-  "base-card": "#F3F2F2",
-  "base-input": "#EDEDED",
-  "base-button": "#E6E5E5",
-  "base-hover": "#D7D5D5",
-  "base-label": "#8D8686",
-  "base-text": "#574F4D",
-  "base-subtitle": "#403937",
-  "base-title": "#272221",
+  color: {
+    // base
+    white: "#FFFFFF",
+    background: "#FAFAFA",
+    "base-card": "#F3F2F2",
+    "base-input": "#EDEDED",
+    "base-button": "#E6E5E5",
+    "base-hover": "#D7D5D5",
+    "base-label": "#8D8686",
+    "base-text": "#574F4D",
+    "base-subtitle": "#403937",
+    "base-title": "#272221",
 
-  // produto
-  purple: "#8047F8",
-  "purple-light": "#EBE5F9",
-  "purple-dark": "#4B2995",
-  "yellow-light": "#F1E9C9",
-  "yellow-product": "#DBAC2C",
-  "yellow-dark": "#C47F17",
+    // produto
+    purple: "#8047F8",
+    "purple-light": "#EBE5F9",
+    "purple-dark": "#4B2995",
+    "yellow-light": "#F1E9C9",
+    "yellow-product": "#DBAC2C",
+    "yellow-dark": "#C47F17",
+  },
+  font: ["Roboto", "Baloo 2", "sans-serif"],
+  fontSizes: {
+    roboto: {
+      Text_L: {
+        fontSize: "1.25rem",
+        lineHeigt: "130%",
+        weght: "Regular & Bold",
+      },
+      Text_M: "1em",
+      Text_S: "0.875rem",
+      Text_XS: "0.75rem",
+      Tag: "0.625rem",
+      Button_G: "0.875rem",
+      Button_M: "0.75rem",
+    },
+    baloo2: {
+      Title_XL: "3rem",
+      Title_L: "2rem",
+      Title_M: "1.5rem",
+      Title_S: "1.25rem",
+      Title_XS: "1.125rem",
+    },
+  },
 };
